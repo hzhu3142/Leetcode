05a94f518c454d91679a73512c2d4f89371ac255
hzhu3142 <69029054+hzhu3142@users.noreply.github.com>
2021-05-25 17:51:33 -0700

    Update 1368.py

--- 1368.py
+++ 1368.py
@@ -1,3 +1,31 @@
+#DP
+class Solution:
+    def minCost(self, A):
+        n, m, inf, k = len(A), len(A[0]), 10**9, 0
+        dp = [[inf] * m for i in range(n)]
+        dirt = [[0, 0], [0, 1], [0, -1], [1, 0], [-1, 0]]
+        bfs = []
+
+        def dfs(x, y):
+            if not (0 <= x < n and 0 <= y < m and dp[x][y] == inf): 
+                return
+            dp[x][y] = k
+            bfs.append([x, y])
+            a, b = dirt[A[x][y]]
+            dfs(x + a, y + b)
+
+        dfs(0, 0)
+        while bfs:
+            k += 1
+            bfs, bfs2 = [], bfs
+            for x, y in bfs2:
+                for i, j in dirt[1:]:
+                    dfs(x + i, y + j)
+        return dp[-1][-1]
+				
+				
+				
+#dijkstra
 class Solution:
     def minCost(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
