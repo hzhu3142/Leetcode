c30a1f5b48bb29ca799c139387f592183148294a
hzhu3142 <69029054+hzhu3142@users.noreply.github.com>
2021-06-09 16:27:49 -0700

    Update 324.py

--- 324.py
+++ 324.py
@@ -9,33 +9,47 @@ class Solution:
         
         
         
-#quick select + virtual index
+#quick selections + virtual index
 import random
 class Solution:
     def wiggleSort(self, nums: List[int]) -> None:
-        def quickSelect(nums,n):            
-            start,end=0,len(nums)-1
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        mid = (n+1)//2
+        
+        def quickSort():
+            start, end = 0, n-1
             while True:
-                pivot=nums[random.randint(start,end)]
-                i,j,k=start,end,start
-                while k<=j:
-                    if nums[k]<pivot:
-                        nums[i],nums[k]=nums[k],nums[i]
-                        i+=1
-                        k+=1
-                    elif nums[k]>pivot:
-                        nums[j],nums[k]=nums[k],nums[j]
-                        j-=1
+                pivot = nums[random.randint(start, end)]
+                l = cur = start
+                r = end
+                while cur <= r:
+                    if nums[cur] < pivot:
+                        nums[l], nums[cur] = nums[cur], nums[l]
+                        l += 1
+                        cur += 1
+                    elif nums[cur] > pivot:
+                        nums[cur], nums[r] = nums[r], nums[cur]
+                        r -= 1
                     else:
-                        k+=1
-                if i<=n-1<=j:
-                    return pivot
-                elif n-1<i:
-                    end=i-1
-                else:
-                    start=i+1
-        n=len(nums)
-        mid=quickSelect(nums,(n+1)//2)
+                        cur += 1
+            
+                if nums[mid-1] == pivot:
+                    return 
+                elif r+1 > mid:
+                    end = r - 1
+                elif r+1 < mid:
+                    start = r + 1
+        
+       
+        quickSort()
+        # print(nums)
+
+        
+        # nums[::2], nums[1::2] = nums[:mid][::-1], nums[mid:][::-1]
+        # virtual index
         mapIdx=lambda i:(1+2*i)%(n|1)
         i,j,k=0,n-1,0
         while k<=j:
