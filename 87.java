e41931fd1261afffd33c13c8a2ee77953f6666ab
hzhu3142 <69029054+hzhu3142@users.noreply.github.com>
2021-06-24 19:39:46 -0700

    Update 87.java

--- 87.java
+++ 87.java
@@ -1,32 +1,39 @@
 class Solution {
 
-    final Map<String, Boolean> cache = new HashMap<>();
+    final Map<String, Boolean> cache = new HashMap<>();  // final: can't change??
 
     public boolean isScramble(final String s1, final String s2) {
-        if (s1.equals(s2)) return true; 
         
-        int[] letters = new int[26];
-        for (int i=0; i<s1.length(); i++) {
-            letters[s1.charAt(i)-'a']++;
-            letters[s2.charAt(i)-'a']--;
-        }
-        for (int i=0; i<26; i++) if (letters[i]!=0) return false;
-    
-
-        String key = s1+"____"+s2;
+        String key = s1+s2;
 
         if (cache.containsKey(key)) {
             return cache.get(key);
         }
+        
+        if (s1.equals(s2)) return true; 
+        
+        int[] freq = new int[26];
+        for (int i=0; i<s1.length(); i++) {
+            freq[s1.charAt(i)-'a']++;
+            freq[s2.charAt(i)-'a']--;
+        }
+        for (int i=0; i<26; i++) if (freq[i]!=0) return false;
 
         for (int i = 1; i < s1.length(); i++) {
-            final String s2SubstringLeft = s2.substring(0, i);
-            final String s2SubstringRight = s2.substring(i);
+            
+            final String s1left = s1.substring(0, i);
+            final String s1right = s1.substring(i);
+            final String s2left = s2.substring(0, i);
+            final String s2right = s2.substring(i);
 
-            if ((isScramble(s1.substring(0, i), s2SubstringLeft)
-                    && isScramble(s1.substring(i), s2SubstringRight)) ||
-                    (isScramble(s1.substring(s1.length() - i), s2SubstringLeft)
-                            && isScramble(s1.substring(0, s1.length() - i), s2SubstringRight))) {
+            if (isScramble(s1left, s2left)
+                    && isScramble(s1right, s2right)) {
+                cache.put(key, true);
+                return true;
+            }
+            
+            if  (isScramble(s1right, s2.substring(0, s1.length() - i))
+                            && isScramble(s1left, s2.substring(s1.length() - i))) {
 
                 cache.put(key, true);
                 return true;
@@ -36,4 +43,4 @@ class Solution {
         cache.put(key, false);
         return false;
     }
-}    
+}       
